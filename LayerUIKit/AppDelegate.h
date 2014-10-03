8a15ac4546bce9db8bec8557ed5e0418f1451aae
Kevin Coleman <kcoleman731@gmail.com>
2014-10-03 14:48:00 -0700

    Organizing project

---
+++ LayerUIKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LayerUIKit
+//
+//  Created by Kevin Coleman on 10/3/14.
+//  Copyright (c) 2014 Layer, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
