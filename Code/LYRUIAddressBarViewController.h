1c76f01608cfbacef445f8bb62c7a59d832ce9da
Ben Blakley <brblakley@gmail.com>
2014-12-02 16:24:03 -0800

    Fixed typos in address bar view controller header documentation.

--- Code/LYRUIAddressBarViewController.h
+++ Code/LYRUIAddressBarViewController.h
@@ -28,23 +28,23 @@
 /**
  @abstract Informs the delegate that the user made a participant selection.
  @param addressBarViewController The `LYRUIAddressBarViewController` in which the selection occurred.
- @param participant The participant that was selected and added to the address bar.
- @discussion Upon selection, the participant's full name will be appended ot any existing text in the `LYRUIAddressBarTextView`.
+ @param participant The participant who was selected and added to the address bar.
+ @discussion Upon selection, the participant's full name will be appended to any existing text in the `LYRUIAddressBarTextView`.
  The set of participants represents the identifiers for all currently displayed participants.
  */
 - (void)addressBarViewController:(LYRUIAddressBarViewController *)addressBarViewController didSelectParticipant:(id<LYRUIParticipant>)participant;
 
 /**
- @abstract Informs the delegate that the user removed a participant for the address bar.
+ @abstract Informs the delegate that the user removed a participant from the address bar.
  @param addressBarViewController The `LYRUIAddressBarViewController` in which the removal occurred.
- @param participant The participants that was removed.
+ @param participant The participant who was removed.
  */
 - (void)addressBarViewController:(LYRUIAddressBarViewController *)addressBarViewController didRemoveParticipant:(id<LYRUIParticipant>)participant;
 
 /**
  @abstract Informs the delegate that the user finished searching.
  @param addressBarViewController The `LYRUIAddressBarViewController` in which the search occurred.
- @discussion Searching ends when the user either selectes a participant or removes all pariticipants from the `LYRUIAddressBarTextView`.
+ @discussion Searching ends when the user either selects a participant or removes all participants from the `LYRUIAddressBarTextView`.
  */
 - (void)addressBarViewControllerDidEndSearching:(LYRUIAddressBarViewController *)addressBarViewController;
 
@@ -69,7 +69,7 @@
  @abstract Asks the data source for an NSSet of participants given a search string.
  @param searchText The text upon which a participant search should be performed.
  @param completion The completion block to be called upon search completion.
- @discussion Search should be performed across `LYRUIPaticipant` object's fullName property.
+ @discussion Search should be performed across each `LYRUIParticipant` object's fullName property.
  */
 - (void)searchForParticipantsMatchingText:(NSString *)searchText completion:(void (^)(NSSet *participants))completion;
 
@@ -78,15 +78,15 @@
 /**
  @abstract The `LYRUIAddressBarViewController` class presents an interface that provides for displaying an address bar
  in an `LYRUIConversationViewController`.
- @discussion The class handles displaying the Address Bar in addition to a table view of participants in response to user search input. 
- When a participant is selected, the class appends that participants's full name to any existing text currently displayed. It also creates
- `LYRUIAddressToken` objects which allow for the entire participant name to be highlighted in repsone to a tap. The bar's design and 
+ @discussion The class handles displaying the address bar in addition to a table view of participants in response to user search input.
+ When a participant is selected, the class appends that participant's full name to any existing text currently displayed. It also creates
+ `LYRUIAddressToken` objects which allow for the entire participant name to be highlighted in response to a tap. The bar's design and
  functionality closely correlates with the design and functionality of the address bar in iMessage.
  */
 @interface LYRUIAddressBarViewController : UIViewController
 
 /**
- @abstract The `LYRUIAddressBarControllerDelegate` class informs the reciever to specific events that occurred within the controller.
+ @abstract The `LYRUIAddressBarControllerDelegate` class informs the receiver of specific events that occurred within the controller.
  */
 @property (nonatomic, weak) id<LYRUIAddressBarControllerDelegate> delegate;
 
@@ -113,7 +113,7 @@
 - (void)selectParticipant:(id<LYRUIParticipant>)participant;
 
 /**
- @abstract Informs the receiver to enter a `Permanent` state. When this method is called, user input or search will be disallowed.
+ @abstract Informs the receiver to enter a permanent state. When this method is called, user input and search will be disallowed.
  */
 - (void)setPermanent;
 
