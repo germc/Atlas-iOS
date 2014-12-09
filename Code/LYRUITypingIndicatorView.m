ce1a541a8e5bcaaae8abfeb76c2a434096b41b2c
Ben Blakley <brblakley@gmail.com>
2014-12-09 14:35:23 -0800

    Renamed typing indicator label for brevity.

--- Code/LYRUITypingIndicatorView.m
+++ Code/LYRUITypingIndicatorView.m
@@ -12,7 +12,7 @@
 
 @interface LYRUITypingIndicatorView ()
 
-@property (nonatomic) UILabel *typingIndicatorLabel;
+@property (nonatomic) UILabel *label;
 @property (nonatomic) CAGradientLayer *backgroundGradientLayer;
 
 @end
@@ -31,27 +31,27 @@
         _backgroundGradientLayer = typingIndicatorBackgroundLayer;
         [self.layer addSublayer:typingIndicatorBackgroundLayer];
 
-        _typingIndicatorLabel = [[UILabel alloc] init];
-        _typingIndicatorLabel.translatesAutoresizingMaskIntoConstraints = NO;
-        _typingIndicatorLabel.textColor = [UIColor lightGrayColor];
-        _typingIndicatorLabel.font = LSMediumFont(12);
-        _typingIndicatorLabel.textColor = [UIColor grayColor];
-        _typingIndicatorLabel.numberOfLines = 1;
-        _typingIndicatorLabel.textAlignment = NSTextAlignmentCenter;
-        _typingIndicatorLabel.backgroundColor = [UIColor clearColor];
-        [self addSubview:_typingIndicatorLabel];
+        _label = [[UILabel alloc] init];
+        _label.translatesAutoresizingMaskIntoConstraints = NO;
+        _label.textColor = [UIColor lightGrayColor];
+        _label.font = LSMediumFont(12);
+        _label.textColor = [UIColor grayColor];
+        _label.numberOfLines = 1;
+        _label.textAlignment = NSTextAlignmentCenter;
+        _label.backgroundColor = [UIColor clearColor];
+        [self addSubview:_label];
 
-        [self addConstraint:[NSLayoutConstraint constraintWithItem:self.typingIndicatorLabel attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeLeft multiplier:1.0 constant:8]];
-        [self addConstraint:[NSLayoutConstraint constraintWithItem:self.typingIndicatorLabel attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeRight multiplier:1.0 constant:-8]];
-        [self addConstraint:[NSLayoutConstraint constraintWithItem:self.typingIndicatorLabel attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeTop multiplier:1.0 constant:0]];
-        [self addConstraint:[NSLayoutConstraint constraintWithItem:self.typingIndicatorLabel attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeHeight multiplier:1.0 constant:0]];
+        [self addConstraint:[NSLayoutConstraint constraintWithItem:self.label attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeLeft multiplier:1.0 constant:8]];
+        [self addConstraint:[NSLayoutConstraint constraintWithItem:self.label attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeRight multiplier:1.0 constant:-8]];
+        [self addConstraint:[NSLayoutConstraint constraintWithItem:self.label attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeTop multiplier:1.0 constant:0]];
+        [self addConstraint:[NSLayoutConstraint constraintWithItem:self.label attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeHeight multiplier:1.0 constant:0]];
     }
     return self;
 }
 
 - (void)setText:(NSString *)text
 {
-    self.typingIndicatorLabel.text = text;
+    self.label.text = text;
 }
 
 - (void)layoutSubviews
