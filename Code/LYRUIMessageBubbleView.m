7e93ce62e8e8b0422286f2d1f764172ba161be2e
Ben Blakley <brblakley@gmail.com>
2014-12-10 15:27:26 -0800

    Removed unnecessarily setting bubble label background to default.

--- Code/LYRUIMessageBubbleView.m
+++ Code/LYRUIMessageBubbleView.m
@@ -30,7 +30,6 @@
         self.clipsToBounds = YES;
 
         self.bubbleViewLabel = [[UILabel alloc] init];
-        self.bubbleViewLabel.backgroundColor = [UIColor clearColor];
         self.bubbleViewLabel.lineBreakMode = NSLineBreakByWordWrapping;
         self.bubbleViewLabel.numberOfLines = 0;
         self.bubbleViewLabel.textColor = [UIColor greenColor];
