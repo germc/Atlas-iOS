0ceb579381f5767300af0055569f2f77e256c6ff
Ben Blakley <brblakley@gmail.com>
2014-12-17 10:00:17 -0800

    Fixed typos in conversation section header documentation comments.

--- Code/LYRUIConversationCollectionViewHeader.h
+++ Code/LYRUIConversationCollectionViewHeader.h
@@ -10,20 +10,20 @@
 
 /**
  @abstract The `LYRUIConversationCollectionViewHeader` class provides support for displaying
- dates and Sender names above message cells.
+ dates and sender names above message cells.
  */
 @interface LYRUIConversationCollectionViewHeader : UICollectionReusableView
 
 /**
  @abstract Displays a string of text representing a date. The string is horizontally centered in the view.
- @param date The date string to be displayed
+ @param date The date string to be displayed.
  */
 - (void)updateWithAttributedStringForDate:(NSAttributedString *)date;
 
 /**
  @abstract Displays a string of text representing a participant. The string will be horizontally aligned with 
  the left edge of the left edge of the message bubble view.
- @param participantName The string of text to be displayed
+ @param participantName The string of text to be displayed.
  */
 - (void)updateWithAttributedStringForParticipantName:(NSAttributedString *)participantName;
 
