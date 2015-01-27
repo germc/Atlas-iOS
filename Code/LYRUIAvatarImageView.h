0e50830db81bbdf4dd63e1a4159f1bc29e653ff6
Kevin Coleman <kcoleman731@gmail.com>
2015-01-27 10:44:05 -0800

    Typos in avatarImageView docs

--- Code/LYRUIAvatarImageView.h
+++ Code/LYRUIAvatarImageView.h
@@ -36,8 +36,8 @@ extern CGFloat const LYRUIAvatarImageDiameter;
 /*
  @abstract Sets the initials to be displayed in the image view.
  @param fullName The full name string representing a participant in a conversation. 
- @discussion The string supplied will be split into components seperated by `whitespaceCharacterSet`. The first 
- letter of each component will be concatened and displayed as the initials. It is reccomended that the `fullName` string
+ @discussion The string supplied will be split into components separated by `whitespaceCharacterSet`. The first
+ letter of each component will be concatened and displayed as the initials. It is recommended that the `fullName` string
  only consist of a first and last name, separated by one white space.
  */
 - (void)setInitialsForFullName:(NSString *)fullName;
