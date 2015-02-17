c30db421bf865881b9be183cafbcc214598561a6
Klemen Verdnik <klemen.verdnik@gmail.com>
2015-02-17 14:03:23 -0800

    Removed the icon layer from the ATLProgressView, and re-enabled animations for progress changes.

--- Code/Views/ATLProgressView.h
+++ Code/Views/ATLProgressView.h
@@ -5,25 +5,43 @@
 //  Created by Klemen Verdnik on 1/17/15.
 //  Copyright (c) 2015 Layer, Inc. All rights reserved.
 //
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
 
 #import <UIKit/UIKit.h>
 
-typedef NS_ENUM(NSInteger, ATLProgressViewIconStyle) {
-    ATLProgressViewIconStyleNone,
-    ATLProgressViewIconStyleDownload,
-    ATLProgressViewIconStyleUpload,
-    ATLProgressViewIconStyleStop,
-    ATLProgressViewIconStylePause,
-    ATLProgressViewIconStylePlay,
-    ATLProgressViewIconStyleError,
-};
-
+/**
+ @abstract A custom progress view that displays the progress in a circular
+ shape. It includes two visual components: a subtle transparent background 
+ ring as a placholder and a foreground ring represanting the current
+ progress state.
+ 
+ Progress changes can be animated using the `setProgress:animated:` method
+ where a `YES` value has to be passed as the `animated` argument.
+ */
 @interface ATLProgressView : UIView
 
-@property (nonatomic, readonly) double progress;
+/**
+ @abstract Progress in percentage, 0.0f being at 0% and 1.0f being at full 100%.
+ @discussion Use `setPrgoress:animated:` to set the progress's value.
+ */
+@property (nonatomic, readonly) float progress;
 
-@property (nonatomic) ATLProgressViewIconStyle iconStyle;
-
-- (void)setProgress:(double)newProgress animated:(BOOL)animated;
+/**
+ @abstract Sets the `progress` float value.
+ @param newProgress The value the progress will be set to.
+ @param animated Pass `YES` to animate the progress change, or `NO` to do an immediate update.
+ */
+- (void)setProgress:(float)newProgress animated:(BOOL)animated;
 
 @end
--- Code/Views/ATLProgressView.m
+++ Code/Views/ATLProgressView.m
@@ -3,7 +3,7 @@
 //  Atlas
 //
 //  Created by Klemen Verdnik on 1/17/15.
-//  Copyright (c) 2014 Layer, Inc. All rights reserved.
+//  Copyright (c) 2015 Layer, Inc. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -20,53 +20,25 @@
 
 #import "ATLProgressView.h"
 
+static NSTimeInterval const ATLProgressViewDefaultBorderWidth = 8.00f;
+static NSTimeInterval const ATLProgressViewDefaultTimeInterval = 0.25f;
+
 float ATLDegreeToRadians(float degrees)
 {
     return ((M_PI * degrees)/ 180);
-}
-
-NSString *ATLProgressViewStringForIconStyle(ATLProgressViewIconStyle iconStyle)
-{
-    switch (iconStyle) {
-        case ATLProgressViewIconStyleDownload:
-            return @"▼";
-
-        case ATLProgressViewIconStyleUpload:
-            return @"▲";
-
-        case ATLProgressViewIconStylePause:
-            return @"❚❚";
-
-        case ATLProgressViewIconStyleStop:
-            return @"⬛︎";
-
-        case ATLProgressViewIconStylePlay:
-            return @"▶︎";
-
-        case ATLProgressViewIconStyleError:
-            return @"✕";
-
-        default:
-            return @"";
-    }
 }
 
 @interface ATLProgressView ()
 
 @property (nonatomic) CAShapeLayer *backRingLayer;
 @property (nonatomic) CAShapeLayer *progressRingLayer;
-@property (nonatomic) CATextLayer *iconLayer;
+@property (nonatomic) UIColor *defaultBackgroundRingColor;
+@property (nonatomic) UIColor *defaultForegroundRingColor;
 @property (nonatomic) UIBezierPath *progressArcPath;
 @property (nonatomic) CGFloat borderWidth;
 @property (nonatomic, readonly) CGFloat radius;
 @property (nonatomic) NSTimeInterval animationDuration;
-@property (nonatomic) double progress;
-@property (nonatomic) UIColor *backgroundRingColorUpload;
-@property (nonatomic) UIColor *foregroundRingColorUpload;
-@property (nonatomic) UIColor *iconColorUpload;
-@property (nonatomic) UIColor *backgroundRingColorDownload;
-@property (nonatomic) UIColor *foregroundRingColorDownload;
-@property (nonatomic) UIColor *iconColorDownload;
+@property (nonatomic) float progress;
 
 @end
 
@@ -92,21 +64,15 @@ NSString *ATLProgressViewStringForIconStyle(ATLProgressViewIconStyle iconStyle)
 
 - (void)lyr_commonInit
 {
-    _borderWidth = 8.0f;
-    _progress = 0.0f;
-    _animationDuration = 0.25f;
+    _borderWidth = ATLProgressViewDefaultBorderWidth;
+    _animationDuration = ATLProgressViewDefaultTimeInterval;
     _backRingLayer = [CAShapeLayer layer];
     _progressRingLayer = [CAShapeLayer layer];
-    _iconLayer = [CATextLayer layer];
     [self.layer addSublayer:_backRingLayer];
     [self.layer addSublayer:_progressRingLayer];
-    [self.layer addSublayer:_iconLayer];
-    _backgroundRingColorUpload = [UIColor colorWithRed:0.8f green:0.0f blue:0.0f alpha:0.3f];
-    _foregroundRingColorUpload = [UIColor colorWithRed:0.8f green:0.0f blue:0.0f alpha:0.5f];
-    _iconColorUpload = [UIColor colorWithRed:0.8f green:0.0f blue:0.0f alpha:0.3f];
-    _backgroundRingColorDownload = [UIColor colorWithWhite:0.8f alpha:0.7f];
-    _foregroundRingColorDownload = [UIColor colorWithWhite:1.0f alpha:0.9f];
-    _iconColorDownload = [UIColor colorWithWhite:0.8f alpha:0.9f];
+    _defaultBackgroundRingColor = [UIColor colorWithRed:0.8f green:0.0f blue:0.0f alpha:0.3f];
+    _defaultForegroundRingColor = [UIColor colorWithRed:0.8f green:0.0f blue:0.0f alpha:0.5f];
+    _progress = 0.0f;
 }
 
 - (void)layoutSubviews
@@ -125,31 +91,16 @@ NSString *ATLProgressViewStringForIconStyle(ATLProgressViewIconStyle iconStyle)
     _progressRingLayer.fillColor = [UIColor clearColor].CGColor;
     _progressRingLayer.position = CGPointMake(self.layer.frame.size.width / 2, self.layer.frame.size.height / 2);
     _progressRingLayer.strokeEnd = self.progress;
+    _progressRingLayer.strokeColor = _defaultForegroundRingColor.CGColor;
 
     _backRingLayer.frame = self.bounds;
-    _backRingLayer.strokeColor = [UIColor colorWithWhite:0.8f alpha:0.5f].CGColor;
     _backRingLayer.lineWidth = self.borderWidth;
     _backRingLayer.path = path.CGPath;
     _backRingLayer.anchorPoint = CGPointMake(0.5, 0.5);
     _backRingLayer.fillColor = [UIColor clearColor].CGColor;
     _backRingLayer.position = CGPointMake(self.layer.frame.size.width / 2, self.layer.frame.size.height / 2);
     _backRingLayer.strokeEnd = 1.0f;
-
-    _iconLayer.frame = CGRectOffset(self.bounds, 0, (self.bounds.size.height / 5.5));
-    _iconLayer.anchorPoint = CGPointMake(0.5, 0.5);
-    _iconLayer.alignmentMode = kCAAlignmentCenter;
-    _iconLayer.opacity = self.iconStyle == ATLProgressViewIconStyleNone ? 0.0f : 1.0f;
-    _iconLayer.string = ATLProgressViewStringForIconStyle(self.iconStyle);
-
-    if (self.iconStyle == ATLProgressViewIconStyleUpload) {
-        _backRingLayer.strokeColor = _backgroundRingColorUpload.CGColor;
-        _progressRingLayer.strokeColor = _foregroundRingColorUpload.CGColor;
-        _iconLayer.foregroundColor = _iconColorUpload.CGColor;
-    } else {
-        _backRingLayer.strokeColor = _backgroundRingColorDownload.CGColor;
-        _progressRingLayer.strokeColor = _foregroundRingColorDownload.CGColor;
-        _iconLayer.foregroundColor = _iconColorDownload.CGColor;
-    }
+    _backRingLayer.strokeColor = _defaultBackgroundRingColor.CGColor;
 }
 
 - (CGFloat)radius
@@ -157,52 +108,22 @@ NSString *ATLProgressViewStringForIconStyle(ATLProgressViewIconStyle iconStyle)
     return MIN(self.bounds.size.width, self.bounds.size.height);
 }
 
-- (void)setProgress:(double)newProgress animated:(BOOL)animated
+- (void)setProgress:(float)newProgress animated:(BOOL)animated
 {
+    // Animate only if the animation is request, and if the new value
+    // is bigger than the previous one.
+    if (animated && (newProgress > self.progress)) {
+        CABasicAnimation *strokeEndAnimation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
+        strokeEndAnimation.duration = self.animationDuration;
+        [strokeEndAnimation setFillMode:kCAFillModeForwards];
+        strokeEndAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
+        strokeEndAnimation.removedOnCompletion = YES;
+        strokeEndAnimation.fromValue = [NSNumber numberWithFloat:self.progress];
+        strokeEndAnimation.toValue = [NSNumber numberWithFloat:newProgress];
+        [self.progressRingLayer addAnimation:strokeEndAnimation forKey:@"progressStatus"];
+    }
     self.progressRingLayer.strokeEnd = newProgress;
     _progress = newProgress;
-}
-
-- (void)setIconStyle:(ATLProgressViewIconStyle)iconStyle
-{
-    // Set the color based on the icon type (it it's upload or anything else)
-    if (iconStyle == ATLProgressViewIconStyleUpload) {
-        _backRingLayer.strokeColor = _backgroundRingColorUpload.CGColor;
-        _progressRingLayer.strokeColor = _foregroundRingColorUpload.CGColor;
-        _iconLayer.foregroundColor = _iconColorUpload.CGColor;
-    } else {
-        _backRingLayer.strokeColor = _backgroundRingColorDownload.CGColor;
-        _progressRingLayer.strokeColor = _foregroundRingColorDownload.CGColor;
-        _iconLayer.foregroundColor = _iconColorDownload.CGColor;
-    }
-
-    if (self.iconStyle == ATLProgressViewIconStyleNone && iconStyle != ATLProgressViewIconStyleNone) {
-        CABasicAnimation *fadeInAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
-        fadeInAnimation.duration = self.animationDuration;
-        fadeInAnimation.fillMode = kCAFillModeForwards;
-        fadeInAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
-        fadeInAnimation.removedOnCompletion = NO;
-        fadeInAnimation.fromValue = [NSNumber numberWithFloat:0.0f];
-        fadeInAnimation.toValue = [NSNumber numberWithFloat:1.0f];
-        self.iconLayer.opacity = 1.0f;
-        [self.iconLayer addAnimation:fadeInAnimation forKey:@"fadeIn"];
-    } else if (self.iconStyle != ATLProgressViewIconStyleNone && iconStyle == ATLProgressViewIconStyleNone) {
-        CABasicAnimation *fadeOutAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
-        fadeOutAnimation.duration = self.animationDuration;
-        fadeOutAnimation.fillMode = kCAFillModeForwards;
-        fadeOutAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
-        fadeOutAnimation.removedOnCompletion = NO;
-        fadeOutAnimation.fromValue = [NSNumber numberWithFloat:1.0f];
-        fadeOutAnimation.toValue = [NSNumber numberWithFloat:0.0f];
-        self.iconLayer.opacity = 0.0f;
-        [self.iconLayer addAnimation:fadeOutAnimation forKey:@"fadeOut"];
-    }
-    
-    NSString *iconString = ATLProgressViewStringForIconStyle(iconStyle);
-    if (iconStyle) {
-        self.iconLayer.string = iconString;
-    }
-    _iconStyle = iconStyle;
 }
 
 @end
