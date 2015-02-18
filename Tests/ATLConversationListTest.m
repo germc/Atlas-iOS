c608de0067b91a980ceff4000d64ba803369348e
Blake Watters <blake@layer.com>
2015-02-17 17:47:46 -0800

    Audit Apache 2 boilerplate in tests dir

--- Tests/ATLConversationListTest.m
+++ Tests/ATLConversationListTest.m
@@ -17,6 +17,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 //
+
 #import <XCTest/XCTest.h>
 #import <Atlas/Atlas.h>
 #import "ATLTestInterface.h"
