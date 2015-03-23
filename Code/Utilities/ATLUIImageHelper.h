affa7c1a3209ed751571696f920fe7b6a54bff4e
Kabir Mahal <kabirmahal@gmail.com>
2015-03-22 22:23:21 -0700

    added in UIImage helper for GIF handling

---
+++ Code/Utilities/ATLUIImageHelper.h
@@ -0,0 +1,16 @@
+//
+//  ATLUIImageHelper.h
+//  Pods
+//
+//  Created by Kabir Mahal on 3/18/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface ATLUIImageHelper : NSObject
+
++ (UIImage *)animatedImageWithAnimatedGIFData:(NSData *)data;
+
+@end
