c38e0dc00fd8ab6423a717426a7edc0686e88f31
Kabir Mahal <kabirmahal@gmail.com>
2015-03-24 10:38:19 -0700

    fixed ATLUIImageHelper class with improved method names, formatting, and licensing information

--- Code/Utilities/ATLUIImageHelper.m
+++ Code/Utilities/ATLUIImageHelper.m
@@ -4,7 +4,8 @@
 //
 //  Created by Kabir Mahal on 3/18/15.
 //
-//
+//  Credit and source to: https://github.com/mayoff/uiimage-from-animated-gif
+
 
 #import "ATLUIImageHelper.h"
 #import <ImageIO/ImageIO.h>
@@ -19,19 +20,22 @@
 
 @implementation ATLUIImageHelper
 
-static int delayCentisecondsForImageAtIndex(CGImageSourceRef const source, size_t const i) {
+#pragma mark - Private Methods
+
+static int ATLDelayCentisecondsForImageAtIndex(CGImageSourceRef const source, size_t const index)
+{
     int delayCentiseconds = 1;
-    CFDictionaryRef const properties = CGImageSourceCopyPropertiesAtIndex(source, i, NULL);
+    CFDictionaryRef const properties = CGImageSourceCopyPropertiesAtIndex(source, index, NULL);
     if (properties) {
         CFDictionaryRef const gifProperties = CFDictionaryGetValue(properties, kCGImagePropertyGIFDictionary);
         if (gifProperties) {
-            NSNumber *number = fromCF CFDictionaryGetValue(gifProperties, kCGImagePropertyGIFUnclampedDelayTime);
-            if (number == NULL || [number doubleValue] == 0) {
-                number = fromCF CFDictionaryGetValue(gifProperties, kCGImagePropertyGIFDelayTime);
+            NSNumber *gifFrameDuration = fromCF CFDictionaryGetValue(gifProperties, kCGImagePropertyGIFUnclampedDelayTime);
+            if (gifFrameDuration == NULL || [gifFrameDuration doubleValue] == 0) {
+                gifFrameDuration = fromCF CFDictionaryGetValue(gifProperties, kCGImagePropertyGIFDelayTime);
             }
-            if ([number doubleValue] > 0) {
+            if ([gifFrameDuration doubleValue] > 0) {
                 // Even though the GIF stores the delay as an integer number of centiseconds, ImageIO “helpfully” converts that to seconds for us.
-                delayCentiseconds = (int)lrint([number doubleValue] * 100);
+                delayCentiseconds = (int)lrint([gifFrameDuration doubleValue] * 100);
             }
         }
         CFRelease(properties);
@@ -39,14 +43,16 @@ static int delayCentisecondsForImageAtIndex(CGImageSourceRef const source, size_
     return delayCentiseconds;
 }
 
-static void createImagesAndDelays(CGImageSourceRef source, size_t count, CGImageRef imagesOut[count], int delayCentisecondsOut[count]) {
+static void ATLCreateImagesAndDelays(CGImageSourceRef source, size_t count, CGImageRef imagesOut[count], int delayCentisecondsOut[count])
+{
     for (size_t i = 0; i < count; ++i) {
         imagesOut[i] = CGImageSourceCreateImageAtIndex(source, i, NULL);
-        delayCentisecondsOut[i] = delayCentisecondsForImageAtIndex(source, i);
+        delayCentisecondsOut[i] = ATLDelayCentisecondsForImageAtIndex(source, i);
     }
 }
 
-static int sum(size_t const count, int const *const values) {
+static int ATLSum(size_t const count, int const *const values)
+{
     int theSum = 0;
     for (size_t i = 0; i < count; ++i) {
         theSum += values[i];
@@ -54,29 +60,32 @@ static int sum(size_t const count, int const *const values) {
     return theSum;
 }
 
-static int pairGCD(int a, int b) {
-    if (a < b)
-        return pairGCD(b, a);
+static int ATLPairGCD(int duration, int gcd)
+{
+    if (duration < gcd)
+        return ATLPairGCD(gcd, duration);
     while (true) {
-        int const r = a % b;
+        int const r = duration % gcd;
         if (r == 0)
-            return b;
-        a = b;
-        b = r;
+            return gcd;
+        duration = gcd;
+        gcd = r;
     }
 }
 
-static int vectorGCD(size_t const count, int const *const values) {
+static int ATLVectorGCD(size_t const count, int const *const values)
+{
     int gcd = values[0];
     for (size_t i = 1; i < count; ++i) {
         // Note that after I process the first few elements of the vector, `gcd` will probably be smaller than any remaining element.  By passing the smaller value as the second argument to `pairGCD`, I avoid making it swap the arguments.
-        gcd = pairGCD(values[i], gcd);
+        gcd = ATLPairGCD(values[i], gcd);
     }
     return gcd;
 }
 
-static NSArray *frameArray(size_t const count, CGImageRef const images[count], int const delayCentiseconds[count], int const totalDurationCentiseconds) {
-    int const gcd = vectorGCD(count, delayCentiseconds);
+static NSArray *ATLFrameArray(size_t const count, CGImageRef const images[count], int const delayCentiseconds[count], int const totalDurationCentiseconds)
+{
+    int const gcd = ATLVectorGCD(count, delayCentiseconds);
     size_t const frameCount = totalDurationCentiseconds / gcd;
     UIImage *frames[frameCount];
     for (size_t i = 0, f = 0; i < count; ++i) {
@@ -88,27 +97,30 @@ static NSArray *frameArray(size_t const count, CGImageRef const images[count], i
     return [NSArray arrayWithObjects:frames count:frameCount];
 }
 
-static void releaseImages(size_t const count, CGImageRef const images[count]) {
+static void ATLReleaseImages(size_t const count, CGImageRef const images[count])
+{
     for (size_t i = 0; i < count; ++i) {
         CGImageRelease(images[i]);
     }
 }
 
-static UIImage *animatedImageWithAnimatedGIFImageSource(CGImageSourceRef const source) {
+static UIImage *ATLAnimatedImageWithAnimatedGIFImageSource(CGImageSourceRef const source)
+{
     size_t const count = CGImageSourceGetCount(source);
     CGImageRef images[count];
     int delayCentiseconds[count]; // in centiseconds
-    createImagesAndDelays(source, count, images, delayCentiseconds);
-    int const totalDurationCentiseconds = sum(count, delayCentiseconds);
-    NSArray *const frames = frameArray(count, images, delayCentiseconds, totalDurationCentiseconds);
+    ATLCreateImagesAndDelays(source, count, images, delayCentiseconds);
+    int const totalDurationCentiseconds = ATLSum(count, delayCentiseconds);
+    NSArray *const frames = ATLFrameArray(count, images, delayCentiseconds, totalDurationCentiseconds);
     UIImage *const animation = [UIImage animatedImageWithImages:frames duration:(NSTimeInterval)totalDurationCentiseconds / 100.0];
-    releaseImages(count, images);
+    ATLReleaseImages(count, images);
     return animation;
 }
 
-static UIImage *animatedImageWithAnimatedGIFReleasingImageSource(CGImageSourceRef CF_RELEASES_ARGUMENT source) {
+static UIImage *ATLAnimatedImageWithAnimatedGIFReleasingImageSource(CGImageSourceRef CF_RELEASES_ARGUMENT source)
+{
     if (source) {
-        UIImage *const image = animatedImageWithAnimatedGIFImageSource(source);
+        UIImage *const image = ATLAnimatedImageWithAnimatedGIFImageSource(source);
         CFRelease(source);
         return image;
     } else {
@@ -116,13 +128,15 @@ static UIImage *animatedImageWithAnimatedGIFReleasingImageSource(CGImageSourceRe
     }
 }
 
+#pragma mark - Public Helper Methods
+
 + (UIImage *)animatedImageWithAnimatedGIFData:(NSData *)data
 {
-    return animatedImageWithAnimatedGIFReleasingImageSource(CGImageSourceCreateWithData(toCF data, NULL));
+    return ATLAnimatedImageWithAnimatedGIFReleasingImageSource(CGImageSourceCreateWithData(toCF data, NULL));
 }
 
 + (UIImage *)animatedImageWithAnimatedGIFURL:(NSURL *)url {
-    return animatedImageWithAnimatedGIFReleasingImageSource(CGImageSourceCreateWithURL(toCF url, NULL));
+    return ATLAnimatedImageWithAnimatedGIFReleasingImageSource(CGImageSourceCreateWithURL(toCF url, NULL));
 }
 
 @end
