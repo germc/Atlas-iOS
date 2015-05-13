840ad79044f6eb47f9e92e2a188ea2bdbf5afe0b
Kabir Mahal <kabirmahal@gmail.com>
2015-05-13 13:37:47 -0700

    Updated documentation

--- Code/Controllers/ATLConversationViewController.h
+++ Code/Controllers/ATLConversationViewController.h
@@ -75,11 +75,11 @@
  @return An `NSOrderedSet` of `LYRMessage` objects. If `nil` is returned, the controller will fall back to default behavior. If an empty
  `NSOrderedSet` is returned, the controller will not send any messages.
  @discussion Called when a user taps the `SEND` button on an `ATLMessageInputToolbar`. The media attachments array supplied can contain
- any media type, such as text, images, GPS location information. Applications who wish to send `LYRMessage` objects with custom `LYRMessagePart`
- MIME types not supported by default by Atlas can do so by implementing this method. All `LYRMessage` objects returned will be immediately 
- sent into the current conversation for the controller. If implemented, applications should also register custom `UICollectionViewCell` classes 
- with the controller via a call to `registerClass:forMessageCellWithReuseIdentifier:`. They should also implement the optional data source method,
- `conversationViewController:reuseIdentifierForMessage:`.
+ any media type, such as text, images, GPS location information.  The media attachments will be empty when the location icon is tapped. Applications
+ who wish to send `LYRMessage` objects with custom `LYRMessagePart` MIME types not supported by default by Atlas can do so by implementing this method.
+ All `LYRMessage` objects returned will be immediately sent into the current conversation for the controller. If implemented, applications should also
+ register custom `UICollectionViewCell` classes with the controller via a call to `registerClass:forMessageCellWithReuseIdentifier:`. They should
+ also implement the optional data source method, `conversationViewController:reuseIdentifierForMessage:`.
  */
 - (NSOrderedSet *)conversationViewController:(ATLConversationViewController *)viewController messagesForMediaAttachments:(NSArray *)mediaAttachments;
 
